02386072dd8d82446b1b2e54610ef302467f22ac
GotchaNL <dinantbos@hotmail.com>
2024-06-03 19:44:57 +0200

    Revert "vault backup: 2024-06-03 19:27:57"
    This reverts commit 0c3a8ed7922588778b38d7c202c577bc177f58ef.

---
+++ components/misc/footer.tsx
@@ -0,0 +1,14 @@
+import PreviewLink from "./preview-link"
+
+const Footer = () => {
+  return (
+    <footer className="bg-neutral-50 border-t border-neutral-200">
+      <div className="container mx-auto px-5">
+        <div className="py-28 flex flex-col lg:flex-row items-center">
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
